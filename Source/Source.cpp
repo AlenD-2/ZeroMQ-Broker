085effd16fd06630c38c7fb37503b1be4b6ebc50
Ali Nadi <alinadi.cp@gmail.com>
2024-02-28 13:30:48 +0330

    Add: random packet generator function added

--- Source/Source.cpp
+++ Source/Source.cpp
@@ -3,24 +3,57 @@
 #include "../Common/zhelpers.hpp"
 
 #include <QDebug>
+#include <QElapsedTimer>
 
 Source::Source(int type)
     : IClient(type)
 {
-
+    _minPackSize = 50;
+    _maxPackSize = 8'000;
 }
 
 Source::Source(zmq::context_t contexts, zmq::socket_t socket)
     : IClient(std::move(contexts), std::move(socket))
 {
+    _minPackSize = 50;
+    _maxPackSize = 8'000;
 }
 
 void Source::start()
 {
-    for( int request = 0 ; request < 100 ; request++)
+    QElapsedTimer time;
+    time.start();
+    for( int request = 0 ; request < 10000 ; request++)
     {
         s_sendmore(_socket, std::string(""));
-        s_send(_socket, std::string("Msg number ")+std::to_string(request));
+        s_send(_socket, _generateRandomPacket());
     }
-    qDebug()<< "sent";
+    qDebug()<< "sent" << "elapced time: " << time.elapsed();
+}
+
+void Source::setMinPackSize(int size)
+{
+    if(size < 0)
+        return;
+    _minPackSize = size;
+}
+
+void Source::setMaxPackSize(int size)
+{
+    if(size < 0)
+        return;
+    _maxPackSize = size;
+}
+
+std::string Source::_generateRandomPacket()
+{
+    std::string result;
+
+    std::call_once(srandFlag, [](){std::srand(std::time(nullptr));});
+
+    // generate random packet size between minimum size and maximum size
+    auto packSize = (_minPackSize+std::rand())%_maxPackSize;
+    result.resize(packSize,'A');
+
+    return result;
 }
--- Source/Source.h
+++ Source/Source.h
@@ -4,6 +4,8 @@
 
 #include <zmq.hpp>
 
+#include <mutex>
+
 class Source : public IClient
 {
 public:
@@ -11,4 +13,14 @@ public:
     Source(zmq::context_t contexts, zmq::socket_t socket);
 
     void start() override;
+
+    void setMinPackSize(int size);
+    void setMaxPackSize(int size);
+
+private:
+    std::string _generateRandomPacket();
+    int _minPackSize;
+    int _maxPackSize;
+
+    std::once_flag srandFlag;
 };
